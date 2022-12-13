cba5874434b415c201f382b097568cd5f90b78f5
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-12-14 00:57:56 +0700

    create: import HomeFeaturedProducts in Home.jxs

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,10 @@
 import HomeHeader from "./Home/HomeHeader/HomeHeader";
+<<<<<<< Updated upstream
 import Footer from "./Footer/Footer";
 
 export { Footer, HomeHeader };
+=======
+import HomeFeaturedProducts from "./Home/HomeFeaturedProducts/HomeFeaturedProducts"
+
+export { HomeHeader, HomeFeaturedProducts };
+>>>>>>> Stashed changes
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+<<<<<<< Updated upstream
 import { Footer, HomeHeader } from "../../components";
 
+=======
+import {HomeHeader,HomeFeaturedProducts} from "../../components/index"
+>>>>>>> Stashed changes
 import "./Home.css";
 
 const Home = () => {
   return (
     <div id="page">
       <HomeHeader />
+<<<<<<< Updated upstream
       <Footer />
+=======
+      <HomeFeaturedProducts/>
+
+>>>>>>> Stashed changes
     </div>
   );
 };
