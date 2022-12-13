1405c75710e76f5d11b8a734f9ba12013050fa7f
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-12-14 00:56:00 +0700

    Fix: Fix home header title width in HomeHeader

--- frontend/src/components/Home/HomeHeader/HomeHeader.js
+++ frontend/src/components/Home/HomeHeader/HomeHeader.js
@@ -10,12 +10,14 @@ function HomeHeader() {
         <div className="home-header-blur">
           <img id="home-header-image" src={HomeHeaderBanner} alt="" />
         </div>
-        <h1 className="home-header-title">SẢN PHẨM MILANO COFFEE</h1>
-        <p className="home-header-text">
-          Chúng tôi muốn bán một sản phẩm cà phê sạch, chất lượng, với giá thành
-          bình dân để người tiêu dùng được tận hưởng chính xác vị của cà phê
-        </p>
-        <button className="home-header-button">TÌM HIỂU THÊM</button>
+        <div className="home-header-container">
+          <h1 className="home-header-title">SẢN PHẨM MILANO COFFEE</h1>
+          <p className="home-header-text">
+            Chúng tôi muốn bán một sản phẩm cà phê sạch, chất lượng, với giá thành
+            bình dân để người tiêu dùng được tận hưởng chính xác vị của cà phê
+          </p>
+          <button className="home-header-button">TÌM HIỂU THÊM</button>
+        </div>
       </header>
     </>
   );
