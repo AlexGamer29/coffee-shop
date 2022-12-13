92311447f16506a343d7cd3a803ac84039426141
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-12-14 01:16:43 +0700

    Edit: Edit css in HomeHeader.css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,39 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
+import { Footer } from "../../components";
 import {HomeHeader,HomeFeaturedProducts} from "../../components/index"
-=======
-<<<<<<< Updated upstream
-import { Footer, HomeHeader } from "../../components";
-
-=======
-import {HomeHeader,HomeFeaturedProducts} from "../../components/index"
->>>>>>> Stashed changes
->>>>>>> master
-=======
-import {Header} from "../../components/index"
->>>>>>> origin/master
 import "./Home.css";
 
 const Home = () => {
   return (
     <div id="page">
-<<<<<<< HEAD
       <HomeHeader />
-<<<<<<< HEAD
       <HomeFeaturedProducts/>
 
-=======
-<<<<<<< Updated upstream
       <Footer />
-=======
-      <HomeFeaturedProducts/>
-
->>>>>>> Stashed changes
->>>>>>> master
-=======
-      <Header />
->>>>>>> origin/master
     </div>
   );
 };
