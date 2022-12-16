98cb2934018012278516ffa000ab17da035abebe
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-12-16 22:49:23 +0700

    Create: Create HomeFeaturedProducts for Home page

--- frontend/src/components/Home/HomeFeaturedProducts/HomeFeaturedProducts.js
+++ frontend/src/components/Home/HomeFeaturedProducts/HomeFeaturedProducts.js
@@ -4,13 +4,86 @@ import "./HomeFeaturedProducts.css"
 function HomeFeaturedProducts() {
   return (
     <>
-    <div id='home-fProducts'>
-      <div className='home-fProducts-container'>
-        <div className='home-products'>
-            
+      <div id='home-fProducts'>
+        <div className='home-fProducts-container'>
+          {/* Parse Product data in the DIV below */}
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+
+          {/* Delete Below */}
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          <div className='home-products'>
+            <div className='home-products-imageContainer'>
+              <img className='home-products-image' src='https://milanocoffee.com.vn/wp-content/uploads/2022/05/ca-phe-da-moka.png' />
+            </div>
+            <div className='home-products-information'>
+              <h1 className='home-products-name'>Cà phê đá</h1>
+              <h1 className='home-products-price'>20 000đ</h1>
+            </div>
+          </div>
+          
         </div>
       </div>
-    </div>
     </>
   );
 }
