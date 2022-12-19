c83d04e752fb1e7a4f8123564783a794e9add1ce
[TUF] AlexGamer29 <sun123vet@gmail.com>
2022-12-19 20:42:38 +0700

    fix(Navbar): wrong link route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,15 @@ function App() {
     <>
       <div className="App">
         <Router>
-          <Navbar/>
+          <Navbar />
           <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/contact" element={<Contact/>}/>
-            <Route exact path="/coffee" element={<Coffee/>}/>
-            <Route exact path="/new-product" element={<NewProduct/>}/>
-            <Route exact path="/menu" element={<Menu/>}/>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/coffee" element={<Coffee />} />
+            <Route exact path="/new-product" element={<NewProduct />} />
+            <Route exact path="/menu" element={<Menu />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </Router>
       </div>
     </>
