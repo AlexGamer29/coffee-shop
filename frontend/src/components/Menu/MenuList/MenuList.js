0574c928a8df965f1fc9ffe615e039cbe2325652
[TUF] AlexGamer29 <sun123vet@gmail.com>
2022-12-19 20:11:28 +0700

    feat(Menu): add MenuList component

--- frontend/src/components/Menu/MenuList/MenuList.js
+++ frontend/src/components/Menu/MenuList/MenuList.js
@@ -5,15 +5,29 @@ import { MenuItem } from "./index";
 function MenuList() {
   return (
     <div className="menu__list__container">
-      <div className="menu__list">
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
+      <div className="menu__list__item">
+        <div className="home-fProducts-category">
+          <h1 className="home-fProducts-category-title">CÀ PHÊ ĐÁ</h1>
+        </div>
+        <div className="menu__list">
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+        </div>
+      </div>
+      <div className="menu__list__item">
+        <div className="home-fProducts-category">
+          <h1 className="home-fProducts-category-title">CÀ PHÊ SỮA</h1>
+        </div>
+        <div className="menu__list">
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+        </div>
       </div>
     </div>
   );
